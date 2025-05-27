d122d63a2768a6cc0fbd18bb25183e96cf56870b
joelsrzalvarez <contactojoelsuarez@gmail.com>
2025-05-27 13:47:54 +0200

    FIX: Added welcome message

--- StillNoDrop.lua
+++ StillNoDrop.lua
@@ -1,3 +1,11 @@
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", function(self, event, addonName)
+    if addonName == "StillNoDrop" then
+        print("|cff00ff00[StillNoDrop]|r Loaded successfully. Type |cffffff00/snd|r to open the addon.")
+    end
+end)
+
 SLASH_SND1 = "/snd"
 SlashCmdList["SND"] = function()
     if SND and SND.ToggleUI then
